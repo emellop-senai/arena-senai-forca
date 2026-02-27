0b686af646a3a19e37fb5329653c5e8f458018ff
emellop-senai <edio.pereira@edu.sc.senai.br>
2026-02-27 19:14:56 -0300

    update enviroment

--- jogo-forca/src/App.jsx
+++ jogo-forca/src/App.jsx
@@ -1,7 +1,7 @@
 import { useState, useEffect, useRef } from 'react';
 import './index.css';
 
-const API_URL = process.env.API_URL
+const API_URL = import.meta.env.VITE_API_URL;
 
 function App() {
   // ==========================================
